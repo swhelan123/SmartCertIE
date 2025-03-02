f49d4c81d6175bb408094304f8f0c08af72151f2
AlexanderMcConnell <alexmcconnell1002@gmail.com>
2025-03-02 03:40:45 +0000

    Update index.js
    checkout session function introduced - this has the success and cancel pages redirecting to the home page which will have to be changed

--- functions/index.js
+++ functions/index.js
@@ -7,11 +7,10 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// const {onRequest} = require("firebase-functions/v2/https");
+const {onRequest} = require("firebase-functions/v2/https");
 // commented the below line out for the moment as it was causing
 // issues because it wasnt being used
-/* const logger =
-require("firebase-functions/logger");*/
+const logger = require("firebase-functions/logger");
 
 // added this instead and
 // checking if it works
@@ -23,3 +22,29 @@ require("firebase-functions/logger");*/
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.createCheckoutSession = functions.https.onRequest(async (req, res) => {
+    // Optionally, you can add a check for the HTTP method here.
+    try {
+      // Create a new checkout session with Stripe
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        mode: 'subscription', // Set the mode to subscription for recurring payments.
+        line_items: [{
+          price: 'price_1Qy1kzGsigejaHFWZKqC600v', // Replace with your actual Price ID from Stripe.
+          quantity: 1,
+        }],
+        // Replace these URLs with your actual success and cancel pages.
+        success_url: 'https://smartcert.ie',
+        cancel_url: 'https://smartcert.ie',
+        //success_url: 'https://your-domain.com/success?session_id={CHECKOUT_SESSION_ID}',
+        //cancel_url: 'https://your-domain.com/cancel',
+      });
+      // Return the session details to the client.
+      res.status(200).json({ sessionId: session.id, url: session.url });
+    } catch (error) {
+      console.error('Error creating checkout session:', error);
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
