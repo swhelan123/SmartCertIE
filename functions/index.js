f685fface0de35ae379e4962854f32876c3569f6
AlexanderMcConnell <alexmcconnell1002@gmail.com>
2025-03-02 04:26:35 +0000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 const functions = require("firebase-functions");
-const stripe = require("stripe")(functions.config().stripe.secret_key);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 // const {onRequest} =
